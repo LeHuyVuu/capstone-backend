e68386d3ab2c925c99aeac31d27087462e85ef74
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-01-20 20:47:02 +0700

    chore: remove test comment

--- capstone-backend.tests/IntegrationTests/Database/DatabaseMappingTests.cs
+++ capstone-backend.tests/IntegrationTests/Database/DatabaseMappingTests.cs
@@ -37,17 +37,16 @@ namespace capstone_backend.tests.IntegrationTests.Database
 
             // Lấy tất cả Entity có trong DbContext
             var entityTypes = db.Model.GetEntityTypes()
-                .Where(e => !e.IsOwned())         // Bỏ qua Owned Type (Value Object)
-                .Where(e => e.GetTableName() != null) // Bỏ qua mấy cái không có bảng
-                .Where(e => e.ClrType != null);   // Bỏ qua Shadow Entities không có class C#
+                .Where(e => !e.IsOwned())
+                .Where(e => e.GetTableName() != null)
+                .Where(e => e.ClrType != null);
 
             foreach (var entity in entityTypes)
             {
-                // Bỏ qua bảng join many-many nếu nó không có class đại diện (tùy logic của mày)
+
                 if (entity.GetTableName() == "collectionvenue_location")
                     continue;
 
-                // --- MAGIC CỦA REFLECTION ---
                 // Mục đích: Gọi hàm CheckEntity<T>(db) với T là kiểu của entity hiện tại
                 var method = this.GetType()
                     .GetMethod(nameof(CheckEntity), BindingFlags.NonPublic | BindingFlags.Instance)!
@@ -60,8 +59,7 @@ namespace capstone_backend.tests.IntegrationTests.Database
                 }
                 catch (Exception ex)
                 {
-                    // Lỗi sẽ nổ ra ở đây nếu tên cột không khớp
-                    // ex.InnerException thường chứa message "Column ... does not exist"
+
                     throw new Exception($"[ERROR] Error Mapping Table '{entity.GetTableName()}' (Class: {entity.ClrType.Name}): {ex.InnerException?.Message ?? ex.Message}", ex);
                 }
             }
@@ -70,7 +68,6 @@ namespace capstone_backend.tests.IntegrationTests.Database
         // Helper method: Ép EF Core thực hiện query SELECT mapping
         private async Task CheckEntity<T>(MyDbContext db) where T : class
         {
-            // AsNoTracking để nhẹ gánh, FirstOrDefaultAsync để gen SQL
             await db.Set<T>().AsNoTracking().FirstOrDefaultAsync();
         }
     }
