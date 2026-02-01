208512dccc5bf3d8bbe83418ec2ef81d685dcb01
LeHuyVuu <lehuyvuok@gmail.com>
2026-02-01 16:42:23 +0700

    feat: add update functionality for venue owner documents (CCCD, Business License)

--- capstone-backend/Business/DTOs/User/VenueOwnerProfileResponse.cs
+++ capstone-backend/Business/DTOs/User/VenueOwnerProfileResponse.cs
@@ -10,4 +10,7 @@ public class VenueOwnerProfileResponse
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
     public string? Address { get; set; }
+    public string? CitizenIdFrontUrl { get; set; }
+    public string? CitizenIdBackUrl { get; set; }
+    public string? BusinessLicenseUrl { get; set; }
 }
