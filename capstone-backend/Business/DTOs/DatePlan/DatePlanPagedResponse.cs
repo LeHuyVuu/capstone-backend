2fbc49e4c12a3e1af3313e142f5476224af21b88
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-02-04 20:42:36 +0700

    feat: add total upcoming date plan

---
+++ capstone-backend/Business/DTOs/DatePlan/DatePlanPagedResponse.cs
@@ -0,0 +1,10 @@
+﻿using capstone_backend.Business.DTOs.Common;
+
+namespace capstone_backend.Business.DTOs.DatePlan
+{
+    public class DatePlanPagedResponse
+    {
+        public PagedResult<DatePlanResponse> PagedResult { get; set; }
+        public int TotalUpcoming { get; set; }
+    }
+}
