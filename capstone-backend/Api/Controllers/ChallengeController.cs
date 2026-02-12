0708828a834f3e5611ec9e100ea6045a2fd9be8a
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-02-12 16:53:16 +0700

    feat: change fields challenge entity

---
+++ capstone-backend/Api/Controllers/ChallengeController.cs
@@ -0,0 +1,20 @@
+﻿using capstone_backend.Business.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace capstone_backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ChallengeController : ControllerBase
+    {
+        private readonly IChallengeService _challengeService;
+
+        public ChallengeController(IChallengeService challengeService)
+        {
+            _challengeService = challengeService;
+        }
+    }
+}
