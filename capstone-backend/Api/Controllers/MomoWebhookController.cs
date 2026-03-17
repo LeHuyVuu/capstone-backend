8f68fce1f7a82ce64b575ffe452b45f3899d6a4e
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-03-17 13:42:44 +0700

    feat: implement ipn momo test

---
+++ capstone-backend/Api/Controllers/MomoWebhookController.cs
@@ -0,0 +1,34 @@
+﻿using capstone_backend.Business.DTOs.Momo;
+using capstone_backend.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace capstone_backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MomoWebhookController : BaseController
+    {
+        private readonly IMomoService _momoService;
+        private readonly ILogger<MomoWebhookController> _logger;
+
+        public MomoWebhookController(IMomoService momoService, ILogger<MomoWebhookController> logger)
+        {
+            _momoService = momoService;
+            _logger = logger;
+        }
+
+        [HttpPost("ipn")]
+        public async Task<IActionResult> HandleMomoIPN([FromBody] MomoIpnRequest request)
+        {
+
+            _logger.LogInformation("Received Momo IPN: {@Request}", JsonSerializer.Serialize(request, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            }));
+
+            return NoContent();
+        }
+    }
+}
---
+++ capstone-backend/Business/DTOs/Momo/MomoIpnRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace capstone_backend.Business.DTOs.Momo
+{
+    public class MomoIpnRequest
+    {
+        public string OrderType { get; set; }
+        public string OrderInfo { get; set; }
+        public string OrderId { get; set; }
+        public bool Amount { get; set; }
+        public string PartnerCode { get; set; }
+        public string TransId { get; set; }
+        public string Signature { get; set; }
+        public string Message { get; set; }
+        public string PayType { get; set; }
+        public string RequestId { get; set; }
+        public string ResultCode { get; set; }
+        public string ResponseTime { get; set; }
+        public long ExtraData { get; set; }
+    }
+}
