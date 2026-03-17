272b6a959764e36fbea6af5d41b2c80f6834ddc0
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-03-17 15:17:58 +0700

    chore: change type of momoipn

--- capstone-backend/Business/DTOs/Momo/MomoIpnRequest.cs
+++ capstone-backend/Business/DTOs/Momo/MomoIpnRequest.cs
@@ -2,18 +2,18 @@
 {
     public class MomoIpnRequest
     {
-        public string OrderType { get; set; }
-        public string OrderInfo { get; set; }
-        public string OrderId { get; set; }
-        public bool Amount { get; set; }
-        public string PartnerCode { get; set; }
-        public string TransId { get; set; }
-        public string Signature { get; set; }
-        public string Message { get; set; }
-        public string PayType { get; set; }
-        public string RequestId { get; set; }
-        public string ResultCode { get; set; }
-        public string ResponseTime { get; set; }
-        public long ExtraData { get; set; }
+        public string? OrderType { get; set; }
+        public string? OrderInfo { get; set; }
+        public string? OrderId { get; set; }
+        public long Amount { get; set; }
+        public string? PartnerCode { get; set; }
+        public long TransId { get; set; }
+        public string? Signature { get; set; }
+        public string? Message { get; set; }
+        public string? PayType { get; set; }
+        public string? RequestId { get; set; }
+        public int ResultCode { get; set; }
+        public long ResponseTime { get; set; }
+        public string? ExtraData { get; set; }
     }
 }
