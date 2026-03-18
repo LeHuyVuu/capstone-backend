bf596887fa015aac731ad0111ded85e1e84063ac
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-03-18 15:29:14 +0700

    feat: update external ref to json

---
+++ capstone-backend/Business/DTOs/Momo/MomoTransactionMetadata.cs
@@ -0,0 +1,14 @@
+﻿namespace capstone_backend.Business.DTOs.Momo
+{
+    public class MomoTransactionMetadata
+    {
+        public string? RequestId { get; set; }
+        public string? OrderId { get; set; }
+        public long? TransId { get; set; }
+
+        public string? PayUrl { get; set; }
+        public string? DeepLink { get; set; }
+        public string? QrCodeUrl { get; set; }
+        public string? DeeplinkMiniApp { get; set; }
+    }
+}
