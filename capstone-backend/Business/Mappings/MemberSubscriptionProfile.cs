53d1503b7f482552b540f71639d06b907af04fdc
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-03-18 15:56:14 +0700

    feat: get status of payment

---
+++ capstone-backend/Business/Mappings/MemberSubscriptionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using capstone_backend.Business.DTOs.MemberSubscription;
+using capstone_backend.Data.Entities;
+
+namespace capstone_backend.Business.Mappings
+{
+    public class MemberSubscriptionProfile : Profile
+    {
+        public MemberSubscriptionProfile()
+        {
+            CreateMap<MemberSubscriptionPackage, MemberSubscriptionResponse>();
+        }
+    }
+}
---
+++ capstone-backend/Business/Services/MemberSubscriptionService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using capstone_backend.Business.DTOs.MemberSubscription;
+using capstone_backend.Business.DTOs.Momo;
+using capstone_backend.Business.Interfaces;
+using capstone_backend.Data.Enums;
+using capstone_backend.Extensions.Common;
+
+namespace capstone_backend.Business.Services
+{
+    public class MemberSubscriptionService : IMemberSubscriptionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public MemberSubscriptionService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<MemberSubscriptionResponse> CheckPaymentStatusAsync(int userId, int transactionId)
+        {
+            var member = await _unitOfWork.MembersProfile.GetByUserIdAsync(userId);
+            if (member == null)
+                throw new Exception("Hồ sơ thành viên không tồn tại");
+
+            var tx = await _unitOfWork.Transactions.GetByIdAsync(transactionId);
+            if (tx == null || tx.UserId != userId)
+                throw new Exception("Giao dịch không tồn tại hoặc không thuộc về người dùng");
+
+            var sub = await _unitOfWork.MemberSubscriptionPackages.GetByIdAsync(tx.DocNo);
+            if (sub == null)
+                throw new Exception("Không ghi nhận được gói đăng ký của member");
+
+            var metadata = JsonConverterUtil.DeserializeOrDefault<MomoTransactionMetadata>(tx.ExternalRefCode);
+            var response = _mapper.Map<MemberSubscriptionResponse>(tx);
+            response.PayUrl = metadata?.PayUrl;
+            response.QrCodeUrl = metadata?.QrCodeUrl;
+            response.DeepLink = metadata?.DeepLink;
+            response.DeeplinkMiniApp = metadata?.DeeplinkMiniApp;
+
+            response.MemberSubscriptionId = tx.DocNo;
+            response.StartDate = sub.StartDate;
+            response.EndDate = sub.EndDate;
+            response.IsActive = sub.Status == MemberSubscriptionPackageStatus.ACTIVE.ToString();
+
+            return response;
+        }
+    }
+}
