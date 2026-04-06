ede678fd66e77f1df5a2cfb6082744e39d4bc8a2
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-04-06 16:11:15 +0700

    chore: update field type vpnpay ipn data

--- capstone-backend/Business/DTOs/VNPay/VNPayIpnRequest.cs
+++ capstone-backend/Business/DTOs/VNPay/VNPayIpnRequest.cs
@@ -4,40 +4,17 @@ namespace capstone_backend.Business.DTOs.VNPay
 {
     public class VNPayIpnRequest
     {
-        [JsonPropertyName("vnp_TmnCode")]
-        public string TmnCode { get; set; } = null!;
-
-        [JsonPropertyName("vnp_Amount")]
-        public long Amount { get; set; }
-
-        [JsonPropertyName("vnp_BankCode")]
-        public string BankCode { get; set; } = null!;
-
-        [JsonPropertyName("vnp_BankTranNo")]
-        public string? BankTranNo { get; set; }
-
-        [JsonPropertyName("vnp_CardType")]
-        public string? CardType { get; set; }
-
-        [JsonPropertyName("vnp_PayDate")]
-        public int? PayDate { get; set; }
-
-        [JsonPropertyName("vnp_OrderInfo")]
-        public string OrderInfo { get; set; } = null!;
-
-        [JsonPropertyName("vnp_TransactionNo")]
-        public int TransactionNo { get; set; }
-
-        [JsonPropertyName("vnp_ResponseCode")]
-        public int ResponseCode { get; set; }
-
-        [JsonPropertyName("vnp_TransactionStatus")]
-        public int TransactionStatus { get; set; }
-
-        [JsonPropertyName("vnp_TxnRef")]
-        public string TxnRef { get; set; } = null!;
-
-        [JsonPropertyName("vnp_SecureHash")]
-        public string SecureHash { get; set; } = null!;
+        public string vnp_TmnCode { get; set; } = null!;
+        public string vnp_Amount { get; set; } = null!;
+        public string vnp_BankCode { get; set; } = null!;
+        public string? vnp_BankTranNo { get; set; }
+        public string? vnp_CardType { get; set; }
+        public string? vnp_PayDate { get; set; }
+        public string vnp_OrderInfo { get; set; } = null!;
+        public string vnp_TransactionNo { get; set; } = null!;
+        public string vnp_ResponseCode { get; set; } = null!;
+        public string vnp_TransactionStatus { get; set; } = null!;
+        public string vnp_TxnRef { get; set; } = null!;
+        public string vnp_SecureHash { get; set; } = null!;
     }
 }
