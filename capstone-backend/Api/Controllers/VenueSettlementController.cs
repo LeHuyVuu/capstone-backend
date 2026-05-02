9dbfe50f19bbcc2b0006fd0d9322da02535378de
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2026-05-02 21:07:55 +0700

    feat: implement get commission for settlement voucher

--- capstone-backend/Api/Controllers/VenueSettlementController.cs
+++ capstone-backend/Api/Controllers/VenueSettlementController.cs
@@ -1,5 +1,6 @@
 ﻿using capstone_backend.Business.DTOs.VenueSettlement;
 using capstone_backend.Business.Interfaces;
+using capstone_backend.Data.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,32 @@ namespace capstone_backend.Api.Controllers
     public class VenueSettlementController : BaseController
     {
         private readonly IVenueSettlementService _venueSettlementService;
+        private readonly ISystemConfigService _systemConfigService;
 
-        public VenueSettlementController(IVenueSettlementService venueSettlementService)
+        public VenueSettlementController(IVenueSettlementService venueSettlementService, ISystemConfigService systemConfigService)
         {
             _venueSettlementService = venueSettlementService;
+            _systemConfigService = systemConfigService;
+        }
+
+        /// <summary>
+        /// Get commission percent for venue owner
+        /// </summary>
+        [HttpGet("commission")]
+        public async Task<IActionResult> GetCommissionPercent()
+        {
+            try
+            {
+                var userId = GetCurrentUserId();
+                if (userId == null)
+                    return UnauthorizedResponse();
+                var result = await _systemConfigService.GetByKeyAsync(SystemConfigKeys.VENUE_COMMISSION_PERCENT.ToString());
+                return OkResponse(result, "Lấy phần trăm hoa hồng thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequestResponse(ex.Message);
+            }
         }
 
         /// <summary>
